89f8ea812024a689407639163263b434c6be612c
Cory Craig <coryc257@gmail.com>
2021-07-24 12:52:43 -0400

    Hooked it up to the script processor. Done with this book.

--- 64-8/main.c
+++ 64-8/main.c
@@ -10,6 +10,9 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdlib.h>
+#include <signal.h>
 #include "vim_auto.h"
 #include "read_line.h"
 #include "pty_fork.h"
@@ -19,18 +22,61 @@
 
 struct termios ttyOrig;
 
-int
-main(int argc, char *argv[])
+void write_bytes(int terminal, unsigned char *x, VIM_COMMAND *cmd) {
+	for (x = (cmd->bytes + 1); *x != '\n'; x++) {
+		if (write(terminal, x, 1) != 1) {
+			errExit("write\n");
+		}
+	}
+}
+
+void send_commands(int terminal)
 {
-	int fd, master, terminal, status;
-	char *s1, *s2;
-	pid_t slave;
+	int fd;
 	BUFFERED_STREAM_READER stream;
 	VIM_COMMAND *cmd;
 	unsigned char *x;
-	struct winsize ws;
 	char esc[1];
+
 	esc[0] = 27;
+	fd = open("/home/cory/script.vs", O_RDONLY);
+	stream = buffered_stream_reader_new(fd, 4096);
+
+	while ((cmd = fetch_command(stream)) != NULL) {
+		switch (cmd->bytes[0]) {
+		case '*':
+			if (write(terminal, ":", 1) != 1) {
+				errExit("write\n");
+			}
+			write_bytes(terminal, x, cmd);
+			if (write(terminal, "\n", 1) != 1) {
+				errExit("write\n");
+			}
+			//tcflush(terminal, TCIOFLUSH);
+			break;
+		case '-':
+			write(terminal, "i", 1);
+			write_bytes(terminal, x, cmd);
+			write(terminal, esc, 1);
+			break;
+		case '+':
+			write(terminal, "o", 1);
+			write(terminal, esc, 1);
+			break;
+		default:
+			continue;
+		}
+	}
+}
+
+int
+main(int argc, char *argv[])
+{
+	int fd, master, status;
+	char *s1, *s2;
+	pid_t slave;
+	struct winsize ws;
+
 
 	if (tcgetattr(STDIN_FILENO, &ttyOrig) == -1)
 		errExit("tcgetattr");
@@ -62,10 +108,11 @@ main(int argc, char *argv[])
 
 		break;
 	default:
-		write(master,":w file.txt\n",strlen(":w file.txt\n"));
+		send_commands(master);
+		/*write(master,":w file.txt\n",strlen(":w file.txt\n"));
 		write(master,"idog", strlen("idog"));
 		write(master,esc,1);
-		write(master,":wq\n",strlen(":wq\n"));
+		write(master,":wq\n",strlen(":wq\n"));*/
 
 		waitpid(slave, &status, 0);
 		break;
@@ -74,45 +121,6 @@ main(int argc, char *argv[])
 
 	return 0;
 
-	fd = open("/home/cory/script.vs", O_RDONLY);
-
-	stream = buffered_stream_reader_new(fd, 4096);
-
-	ttySetRaw(STDOUT_FILENO, NULL);
-
-	dup2(STDOUT_FILENO, terminal);
-
-	while((cmd = fetch_command(stream)) != NULL) {
-		switch (cmd->bytes[0]) {
-		case '*':
-			if (write(terminal, ":", 1) != 1) {
-				errExit("write\n");
-			}
-			for (x = (cmd->bytes + 1); *x != '\n'; x++) {
-				if(write(terminal, x, 1) != 1) {
-					errExit("write\n");
-				}
-			}
-			if(write(terminal, "\r", 1) != 1) {
-				errExit("write\n");
-			}
-			if(write(terminal, "\n", 1) != 1) {
-				errExit("write\n");
-			}
-			//tcflush(terminal, TCIOFLUSH);
-			break;
-		case '-':
-			break;
-		case '+':
-			break;
-		default:
-			continue;
-		}
-		//printf("%s", cmd->bytes);
-		while (read(terminal,x,1)) {
-			printf("%c",x);
-		}
-	}
 
 }
 
